3a0f12b8fb1d1e1840878bba82c630fde9342fe3
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-27 15:19:36 -0500

    a great save point, map markers functioning

--- scripts/app.js
+++ scripts/app.js
@@ -3,54 +3,70 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).ready(function() {
   console.log("Let's get coding!");
-  // CODE IN HERE!
+
   getMapData();
 });
 
 
 function getMapData() {
-
   $.ajax( {
     method: 'GET',
     url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson',
-
-    //data: '',
     dataType: 'json',
     success: onSuccess,
     error: onError
   })
 }
-// <div id="info">
-//   <p>M 4.2 - 1km ESE of Fontana, California / 123 hours ago </p>
-//   <p>M 3.1 - 6km SSW of Columbus, Ohio / 77 hours ago </p>
-// </div>
-function onSuccess(jsonReturn) {
-  console.log(jsonReturn.features);
 
-  for (var i = 0; i < jsonReturn.features.length; i++) {
+function onSuccess(jsonReturn) {
+  //console.log(jsonReturn.features);
+
+  let coords = [];
+
+  for (var i=0; i<jsonReturn.features.length; i++) {
+
     let data = jsonReturn.features[i].properties;
+    let latitude = jsonReturn.features[i].geometry.coordinates[0];
+    let longitude = jsonReturn.features[i].geometry.coordinates[1];
+    console.log(latitude + "   " + longitude);
+    coords.push({
+      lat: latitude,
+      lng: longitude
+    });
     let magnitude = data.mag;
     let place = data.place;
     let time = data.time;
     let now = Date.now();
     let timeDifference =  Math.floor((((now - time) / 1000) /60) /60);
-    console.log(magnitude);
-
-
-
     let earthquake = '<p>M ' + magnitude + ', ' + place + ' / ' + timeDifference + ' hours ago </p>';
     $('#info').append(earthquake);
-
-
   }
-
-
-
-
-
-
+  //console.log(coords);
+ buildMap(coords);
 }
 
 function onError() {
   console.log('failure');
+}
+
+function buildMap(coords){
+
+
+  //make a map
+  var map = new google.maps.Map(document.getElementById('map'), {
+   center: {lat: 30.2682, lng: -97.74295},
+   zoom: 3
+  });
+
+  //set all marks from coords
+  for(i=0;i<coords.length;i++){
+
+    var marker = new google.maps.Marker({
+            position: coords[i],
+            map: map
+            icon: 'images/earthquake.png'
+  });
+
+  }
+
 }
