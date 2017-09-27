e9f524699b6dbe5f5177097b8b850bd62fe1058a
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-09-27 16:23:01 -0500

    turning in bonus stuff - app finished.

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,8 @@ function getMapData() {
   })
 }
 
+
+
 function onSuccess(jsonReturn) {
   //console.log(jsonReturn.features);
 
@@ -28,18 +30,42 @@ function onSuccess(jsonReturn) {
     let data = jsonReturn.features[i].properties;
     let latitude = jsonReturn.features[i].geometry.coordinates[0];
     let longitude = jsonReturn.features[i].geometry.coordinates[1];
-    console.log(latitude + "   " + longitude);
     coords.push({
       lat: latitude,
       lng: longitude
     });
-    let magnitude = data.mag;
-    let place = data.place;
+    let magnitude = parseInt(data.mag);
+    let place = data.title;
+    let ofword = 'of';
+    let ofdash = '-';
+    let p = place;
     let time = data.time;
     let now = Date.now();
     let timeDifference =  Math.floor((((now - time) / 1000) /60) /60);
-    let earthquake = '<p>M ' + magnitude + ', ' + place + ' / ' + timeDifference + ' hours ago </p>';
+
+    //city sniffing
+    let city = p.slice(p.indexOf(ofword) + ofword.length);
+    let newcity = '';
+    if(city.indexOf(',')!= -1){
+      newcity= city;
+    } else {
+      newcity = p.slice(p.indexOf(ofdash) + ofdash.length);;
+    }
+
+    let cssclass = '';
+    //if mag > 5.0 make it quake-red
+    if(magnitude>5.0){
+      cssclass = " class='quake-red' ";
+    } else if(magnitude>4.0&&magnitude<=5.0) {
+      cssclass = " class='quake-orange' ";
+    } else if(magnitude<=4.0){
+      cssclass = " class='quake-yellow' ";
+    }
+
+
+    let earthquake = '<div class="wrap"><p><span'+cssclass+'>.</span>Magnitude of  ' + magnitude + ', ' + newcity + ' / ' + timeDifference + ' hours ago </p></div>';
     $('#info').append(earthquake);
+
   }
   //console.log(coords);
  buildMap(coords);
@@ -51,22 +77,20 @@ function onError() {
 
 function buildMap(coords){
 
-
   //make a map
   var map = new google.maps.Map(document.getElementById('map'), {
    center: {lat: 30.2682, lng: -97.74295},
-   zoom: 3
+   zoom: 2
   });
 
   //set all marks from coords
   for(i=0;i<coords.length;i++){
 
     var marker = new google.maps.Marker({
-            position: coords[i],
-            map: map
-            icon: 'images/earthquake.png'
-  });
-
+      position: coords[i],
+      map: map,
+      icon: 'images/earthquake2.png'
+    });
   }
 
 }
